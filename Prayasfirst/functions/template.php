cf7473cbb30ea45b6a7d1e24d6fdb210fd3499f5
Vipul Sankhe <vipulx44@gmail.com>
2017-09-26 16:22:05 +0530

    Added dynamic menu

---
+++ Prayasfirst/functions/template.php
@@ -0,0 +1,21 @@
+<?php 
+
+function nav_main($dbc,$pageid){
+
+$q="SELECT * FROM pages";
+
+$r= mysqli_query($dbc, $q);
+
+while($nav=mysqli_fetch_assoc($r)){
+				
+			?>
+			<li class="nav-item">
+	        	<a class="nav-link <?php if($pageid==$nav['id']){echo "active";} ?>" href="?page=<?php echo $nav['id']; ?>"><?php echo $nav['label']; ?> <span class="sr-only">(current)</span></a>
+	      	</li>
+	    	
+			
+			<?php }
+
+}
+
+?>
---
+++ Prayasfirst/template/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+
+	<div class="container">
+		<span class="text-muted">This is the footer</span>
+	</div>
+
+</footer>
